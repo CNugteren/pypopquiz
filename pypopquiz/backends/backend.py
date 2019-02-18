982d15fd32b1c299e8cbc49596c46cf0632d698f
Sven <sgoossens@gmail.com>
2019-02-18 21:06:50 +0100

    Remove useless-pass statements (pylint warning)

--- pypopquiz/backends/backend.py
+++ pypopquiz/backends/backend.py
@@ -31,39 +31,32 @@ class Backend(abc.ABC):
     @abc.abstractmethod
     def trim(self, start_s: int, end_s: int) -> None:
         """Trims a stream to a given start and end time measured in seconds"""
-        pass
 
     @abc.abstractmethod
     def repeat(self) -> None:
         """Concatenates streams with itself to make a twice as long stream"""
-        pass
 
     @abc.abstractmethod
     def combine(self, other: 'Backend', other_first: bool,
                 crossfade_duration: float = 0) -> None:
         """Combines this stream with another stream"""
-        pass
 
     @abc.abstractmethod
     def fade_in_and_out(self, duration_s: int, video_length_s: int, fade_in: bool = True,
                         fade_out: bool = True) -> None:
         """Adds a fade-in and fade-out to/from black for the audio and video stream"""
-        pass
 
     @abc.abstractmethod
     def scale_video(self) -> None:
         """Scales the video and pads if necessary to the requested dimensions"""
-        pass
 
     @abc.abstractmethod
     def draw_text_in_box(self, video_text: str, length: int, move: bool, top: bool,
                          delay_in_sec: Optional[int] = None) -> None:
         """Draws a semi-transparent box either at the top or bottom and writes text in it, optionally scrolling by"""
-        pass
 
     def draw_text(self, video_text: str, height_fraction: float) -> None:
         """Draws text in the center of the video at a certain height fraction"""
-        pass
 
     @abc.abstractmethod
     def add_audio(self, other: 'Backend') -> None:  # type: ignore
@@ -72,46 +65,37 @@ class Backend(abc.ABC):
     @abc.abstractmethod
     def run(self, file_name: Path, dry_run: bool = False) -> Path:
         """Runs the backend to create the video, applying all the filters"""
-        pass
 
     @classmethod
     def create_empty_stream(cls, duration: int, width: int, height: int) -> 'Backend':
         """Creates a video of a certain duration with a black still image"""
-        pass
 
     @classmethod
     def create_silent_stream(cls, duration: float, width: int, height: int) -> 'Backend':
         """Creates audio of a certain duration with no sound"""
-        pass
 
     @classmethod
     def create_single_image_stream(cls, input_image: Path, duration: int,
                                    width: int, height: int) -> 'Backend':
         """Creates a video of a certain duration with a single still image"""
-        pass
 
     @abc.abstractmethod
     def add_spacer(self, text: str, duration_s: float) -> None:
         """Add a text spacer to the start of the video clip."""
-        pass
 
     @abc.abstractmethod
     def add_silence(self, duration_s: float) -> None:
         """Add a silence of a certain duration the an audio clip."""
-        pass
 
     @abc.abstractmethod
     def reverse(self) -> None:
         """Reverses an entire audio or video clip."""
-        pass
 
     def replace_audio_by_beep(self, interval: Tuple[float, float], freq_hz: int = 1500) -> None:
         """Replace the original audio by a beep in a particular interval."""
-        pass
 
     def overlay_fading_text(self, text: str, interval: Tuple[float, float]):
         """Overlay fading text on the clip."""
-        pass
 
     @staticmethod
     @contextlib.contextmanager
